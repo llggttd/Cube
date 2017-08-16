694815c3970e5b03c118fd21d214bce61cb9506b
0z0ne <lgt_livezone@live.com>
2017-08-16 23:14:49 +0800

    add notification

--- src/top/crazyman/plugins/CubeComponent.java
+++ src/top/crazyman/plugins/CubeComponent.java
@@ -1,26 +1,36 @@
 package top.crazyman.plugins;
 
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
+import com.intellij.notification.Notifications;
 import com.intellij.openapi.components.ApplicationComponent;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.UUID;
+
 public class CubeComponent implements ApplicationComponent {
     public CubeComponent() {
-        System.out.printf("CubeComponent");
+        notify("执行", "CubeComponent");
     }
 
     @Override
     public void initComponent() {
-        System.out.printf("initComponent");
+        notify("执行", "initComponent");
     }
 
     @Override
     public void disposeComponent() {
-        System.out.printf("disposeComponent");
+        notify("执行", "disposeComponent");
     }
 
     @Override
     @NotNull
     public String getComponentName() {
         return "CubeComponent";
+    }
+
+    private void notify(String title, String msg) {
+        Notification notification = new Notification("Cube", title, msg, NotificationType.WARNING);
+        Notifications.Bus.notify(notification);
     }
 }
